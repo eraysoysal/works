67be6490d5fb8e00f17295894a36d0fc9a736b93
eraysoysal <erayso1989@gmail.com>
2015-09-03 03:50:55 +0300

    list

--- resource/assets/js/main.js
+++ resource/assets/js/main.js
@@ -1,1 +1,16 @@
+function advancedSearchShow(){
+	$('.checkWrap .link a').click(function(){
+		if ($(this).parent().hasClass('show')) {
+			$(this).parent().next('ul').stop(true,true).slideUp(500);
+			$(this).parent().removeClass('show');
+		}
+		else{
+			$(this).parent().next('ul').stop(true,true).slideDown(500);
+			$(this).parent().addClass('show');	
+		}
+	});
+}
 
+$(document).ready(function(){
+	advancedSearchShow();
+});
