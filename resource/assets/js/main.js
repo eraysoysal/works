29fb1d0398a23bfa6abaa6f74eb061082dbdfaa3
eraysoysal <erayso1989@gmail.com>
2015-09-15 01:57:04 +0300

    login

--- resource/assets/js/main.js
+++ resource/assets/js/main.js
@@ -21,7 +21,46 @@ function selectBoxInıt(){
 	});
 }
 
+function tboxTitleAnimate(){
+	$('.form .tBox').keypress(function() {
+	 	$(this).siblings('.tBoxTitle').fadeIn(300);
+	});
+	$('.form .tBox').blur(function(){
+		if(!$(this).val().length > 0){
+	 		$(this).siblings('.tBoxTitle').fadeOut(300);
+		}
+	});
+}
+
+function validation(){
+	$("form").validate({
+		ignore: [],
+		rules: {
+			username: {
+				required: true,
+				minlength: 2
+			},
+			password: {
+				required: true,
+				minlength: 2
+			}
+		},
+		messages: {
+			username: {
+				required: firstNameRequired,
+				minlength: firstNameMinLength
+			},
+			password: {
+				required: lastNameRequired,
+				minlength: lastNameMinLength
+			}
+		}
+	});
+}
+
 $(document).ready(function(){
 	advancedSearchShow();
 	selectBoxInıt();
+	tboxTitleAnimate();
+	validation();
 });
