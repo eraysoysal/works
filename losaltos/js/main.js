d197b60bba74c29f149d1c2253deca28f14f8a9a
eraysoysal <erayso1989@gmail.com>
2015-11-24 00:48:17 +0200

    gallery

--- losaltos/js/main.js
+++ losaltos/js/main.js
@@ -9,8 +9,8 @@ function menuAnimate(){
 
 function scrollBarInit(){
 	if ($('.los-altos-menu-wrapper').length) {
-		$('.los-altos-menu-wrapper').mCustomScrollbar({
-			theme: "minimal-dark"
+		$('.los-altos-menu-wrapper').mCustomScrollbar(
+{			theme: "minimal-dark"
 		});
 	}
 }
@@ -28,9 +28,26 @@ function initMaplace(){
 	}
 }
 
+function sliderInit(){
+	if($('#losAltosGallery').length){
+		var slider = $('#losAltosGallery');
+		slider.owlCarousel({
+			items: 1,
+			singleItem: true,
+			autoPlay: false,
+			stopOnHover: true,
+			navigation: true,
+			navigationText: ["",""],
+			pagination: false
+		});
+
+	}
+}
+
 $(document).ready(function(){
 	scrollBarInit();
 	initMaplace();
+	sliderInit();
 
 	$('.menu-right .icon-link').click(function(){
 		menuAnimate();
